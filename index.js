eb2ac3c0b24f08d09e66868766152749b2e47ab7
Alonso Holmes <hello@alonso.io>
2016-06-23 10:34:55 -0700

    only launching queues for workers

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('newrelic');
 process.env.PWD = process.cwd();
 require('dotenv').load();
 
-require('./dist/queues/ImageGenerationQueue');
-require('./dist/queues/tokenExchange');
-require('./dist/queues/orderQueue');
-require('./dist/queues/serviceRequest');
-require('./dist/nom_nom/ActivityNomNom');
-require('./dist/nom_nom/StreamNomNom');
+if (process.env.WORKER) {
+	require('./dist/queues/ImageGenerationQueue');
+	require('./dist/queues/tokenExchange');
+	require('./dist/queues/orderQueue');
+	require('./dist/queues/serviceRequest');
+	require('./dist/nom_nom/ActivityNomNom');
+	require('./dist/nom_nom/StreamNomNom');
+}
+
 require('./dist/server');
